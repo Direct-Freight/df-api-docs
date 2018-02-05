8ec2df43bcf38cb3c7bb64687eabe899f49c85b0
jgabriels <jon@directfreight.com>
2018-02-05 09:45:43 -0600

    added defaults and support for examples

--- spec/code_samples/Php_curl/boards@{board_type}/post.php
+++ spec/code_samples/Php_curl/boards@{board_type}/post.php
@@ -10,7 +10,7 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "{\"alert_id\":false,\"count\":false,\"destination_city\":\"<ADD STRING VALUE>\",\"destination_radius\":0,\"destination_state\":[\"<ADD STRING VALUE>\"],\"item_count\":0,\"load_size\":[\"<ADD STRING VALUE>\"],\"only_new\":false,\"origin_city\":\"<ADD STRING VALUE>\",\"origin_radius\":0,\"origin_state\":[\"<ADD STRING VALUE>\"],\"page_number\":0,\"ship_date\":[\"<ADD STRING VALUE>\"],\"total_items\":0,\"total_pages\":0,\"trailer_type\":[\"<ADD STRING VALUE>\"]}",
+  CURLOPT_POSTFIELDS => "{\"alert_id\":5555,\"count\":false,\"destination_city\":\"chicago\",\"destination_radius\":0,\"destination_state\":[\"<ADD STRING VALUE>\"],\"item_count\":0,\"load_size\":\"Full\",\"only_new\":false,\"origin_city\":\"chillicothe\",\"origin_radius\":0,\"origin_state\":[\"<ADD STRING VALUE>\"],\"page_number\":0,\"ship_date\":[\"<ADD STRING VALUE>\"],\"total_items\":0,\"total_pages\":0,\"trailer_type\":[\"<ADD STRING VALUE>\"]}",
   CURLOPT_HTTPHEADER => array(
     "accept: application/json",
     "content-type: application/json"
