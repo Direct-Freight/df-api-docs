f4ee351552e584400a7a9ee9694ec63908b02c40
jgabriels <jon@directfreight.com>
2018-03-03 12:52:56 -0600

    making unhidden view

--- scripts/build.js
+++ scripts/build.js
@@ -9,21 +9,19 @@ mkdir('-p', 'web_deploy')
 
 cp('-R', 'web/*', 'web_deploy/');
 
-exec('npm run swagger bundle --        -o web_deploy/swagger.json');
-exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
-
 console.log("generating code samples");
-exec('npm run generate-code-samples skip-regenerate');
+exec('npm run generate-code-samples');
 
 exec('npm run swagger bundle --        -o web_deploy/swagger.json');
 exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
-
-console.log("hiding items from public view");
-exec('npm run hide-items skip-regenerate');
-
 
 var SWAGGER_UI_DIST = Path.dirname(require.resolve('swagger-ui'));
 rm('-rf', 'web_deploy/swagger-ui/')
 cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/')
 sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../swagger.json', 'web_deploy/swagger-ui/index.html')
 
+mkdir('-p', 'web_deploy/unhide')
+cp('-R', 'web_deploy/*', 'web_deploy/unhide/');
+
+console.log("hiding items from public view");
+exec('npm run hide-items skip-regenerate');
