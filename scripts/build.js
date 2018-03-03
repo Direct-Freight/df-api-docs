6cab89a780e72c1653214d2437981afcc82a0cc0
jgabriels <jon@directfreight.com>
2018-03-03 08:53:39 -0600

    adding some debug

--- scripts/build.js
+++ scripts/build.js
@@ -9,11 +9,15 @@ mkdir('-p', 'web_deploy')
 
 cp('-R', 'web/*', 'web_deploy/');
 
+console.log("bundling json");
 exec('npm run swagger bundle --        -o web_deploy/swagger.json');
+console.log("bundling yaml");
 exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
 
+console.log("regenerating swagger-ui");
 var SWAGGER_UI_DIST = Path.dirname(require.resolve('swagger-ui'));
 rm('-rf', 'web_deploy/swagger-ui/')
 cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/')
 sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../swagger.json', 'web_deploy/swagger-ui/index.html')
+console.log("done regenerating swagger-ui");
 
