b36260d572523eba95f295daaa75b1342b1ac623
jgabriels <jon@directfreight.com>
2018-03-03 11:16:22 -0600

    removed debug comments

--- scripts/build.js
+++ scripts/build.js
@@ -8,16 +8,16 @@ set('-e');
 mkdir('-p', 'web_deploy')
 
 cp('-R', 'web/*', 'web_deploy/');
+
 console.log("generating code samples");
 exec('npm run generate-code-samples');
+
 console.log("bundling json/yaml");
 exec('npm run swagger bundle --        -o web_deploy/swagger.json');
 exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
 
-console.log("regenerating swagger-ui");
 var SWAGGER_UI_DIST = Path.dirname(require.resolve('swagger-ui'));
 rm('-rf', 'web_deploy/swagger-ui/')
 cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/')
 sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../swagger.json', 'web_deploy/swagger-ui/index.html')
-console.log("done regenerating swagger-ui");
 
