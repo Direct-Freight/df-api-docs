cd84a441691b9eb34ecc572bda997f6d87b1c921
jgabriels <jon@directfreight.com>
2018-03-04 09:53:33 -0600

    changed up build.   added a nocode build

--- scripts/build.js
+++ scripts/build.js
@@ -10,9 +10,6 @@ mkdir('-p', 'web_deploy')
 
 cp('-R', 'web/*', 'web_deploy/');
 
-console.log("generating code samples");
-exec('npm run generate-code-samples');
-
 exec('npm run swagger bundle --        -o web_deploy/swagger.json');
 exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
 
@@ -20,13 +17,27 @@ var SWAGGER_UI_DIST = Path.dirname(require.resolve('swagger-ui'));
 cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/')
 sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../swagger.json', 'web_deploy/swagger-ui/index.html')
 
+//create a version without code but has everything
+mkdir('-p', 'web_deploy/nocode')
+cp('-R', 'web_deploy/*', 'web_deploy/nocode/');
+
+console.log("generating code samples");
+exec('npm run generate-code-samples');
+
+//redubundle with the code
+exec('npm run swagger bundle --        -o web_deploy/swagger.json');
+exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
+
+//create a version that shows everything
 mkdir('-p', 'web_deploy/everything')
 cp('-R', 'web_deploy/*', 'web_deploy/everything/');
 
+
+//make the restlet directory public
 mkdir('-p', 'web_deploy/restlet_studio')
 cp('-R', 'restlet_studio/*', 'web_deploy/restlet_studio');
 
+//create a version that hides stuff from the default view
 console.log("hiding items from public view");
 exec('npm run hide-items skip-regenerate');
-
 
