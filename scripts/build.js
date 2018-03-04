b50e2c52255e8a9f0acb0713976c52c9727f0411
jgabriels <jon@directfreight.com>
2018-03-04 10:21:02 -0600

    fixed runaway recursion

--- scripts/build.js
+++ scripts/build.js
@@ -18,8 +18,9 @@ cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/')
 sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../swagger.json', 'web_deploy/swagger-ui/index.html')
 
 //create a version without code but has everything
-mkdir('-p', 'web_deploy/nocode')
-cp('-R', 'web_deploy/*.*', 'web_deploy/nocode/');
+mkdir('-p', 'web_deploy/nocode/swagger-ui/')
+cp('web_deploy/*.*', 'web_deploy/nocode/');
+cp('-R', 'web_deploy/swagger-ui/', 'web_deploy/nocode/swagger-ui/');
 
 console.log("generating code samples");
 exec('npm run generate-code-samples');
@@ -29,13 +30,14 @@ exec('npm run swagger bundle --        -o web_deploy/swagger.json');
 exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
 
 //create a version that shows everything
-mkdir('-p', 'web_deploy/everything')
-cp('-R', 'web_deploy/*.*', 'web_deploy/everything/');
+mkdir('-p', 'web_deploy/everything/swagger-ui/')
+cp('web_deploy/*.*', 'web_deploy/everything/');
+cp('-R', 'web_deploy/swagger-ui/', 'web_deploy/everything/swagger-ui/');
 
 
 //make the restlet directory public
-mkdir('-p', 'web_deploy/restlet_studio')
-cp('-R', 'restlet_studio/*.*', 'web_deploy/restlet_studio');
+mkdir('-p', 'web_deploy/restlet_studio/')
+cp('restlet_studio/*.*', 'web_deploy/restlet_studio/');
 
 //create a version that hides stuff from the default view
 console.log("hiding items from public view");
