90428a6c32e5d8198be80b792f3b69ad4cd56128
jgabriels <jon@directfreight.com>
2018-03-04 22:48:27 -0600

    created an unmodified for easier diffs

--- scripts/fix_references.js
+++ scripts/fix_references.js
@@ -8,6 +8,17 @@ var filename = process.argv[2];
 //now load that file
 var swagger = YAML.safeLoad(fs.readFileSync(filename, 'utf-8'));
 
+console.log('Saving unmodified yaml:' + filename + '-unmodified');
+fs.writeFile(filename + '-unmodified', YAML.safeDump(swagger, {indent: 2, lineWidth: -1, noRefs: false}), (err) => {
+        if (err) 
+                {
+                console.log('ERROR WRITING: ' + filename + '-unmodified');
+                console.log(err);
+                console.log(process.cwd());
+                }
+        });
+
+
 Object.keys(swagger).forEach(function(level1) {
         //toplevels console.log(level1);
         Object.keys(swagger[level1]).forEach(function(level2) {
@@ -20,10 +31,10 @@ Object.keys(swagger).forEach(function(level1) {
                                 swagger[level1][level2][level3]['parameters'].forEach(function(level5) {
                                         if(level5.name == 'end-user-token')
                                                 {
-                                                //console.log('eut');
-                                                //console.log(swagger[level1][level2][level3]['parameters'][x]);
-                                                swagger[level1][level2][level3]['parameters'][x] = { '$ref': '#/parameters/end-user-token' };
-                                                //console.log(swagger[level1][level2][level3]['parameters'][x]);
+                                                //swagger[level1][level2][level3]['parameters'][x] = { '$ref': '#/parameters/end-user-token' };
+                                                swagger[level1][level2][level3]['parameters'].splice(x,1);
+                                                swagger[level1][level2][level3]['parameters'].unshift({ '$ref': '#/parameters/end-user-token' });
+                                                //swagger[level1][level2][level3]['parameters'].push({ '$ref': '#/parameters/end-user-token' });
                                                 }
                                         x++;
                                         });
@@ -33,7 +44,7 @@ Object.keys(swagger).forEach(function(level1) {
 });
 
 console.log('Saving modified yaml:' + filename + '-modified');
-fs.writeFile(filename + '-modified', YAML.safeDump(swagger, {indent: 2, lineWidth: -1, noRefs: true}), (err) => {
+fs.writeFile(filename + '-modified', YAML.safeDump(swagger, {indent: 2, lineWidth: -1, noRefs: false}), (err) => {
         if (err) 
                 {
                 console.log('ERROR WRITING: ' + filename + '-modified');
