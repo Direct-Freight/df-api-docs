c639ed72544927260337595e5fd56acbfe1d577e
jgabriels <jon@directfreight.com>
2018-03-05 09:38:59 -0600

    changed HIDE= to STAGE=

--- scripts/hide-all.js
+++ scripts/hide-all.js
@@ -29,9 +29,9 @@ Object.keys(swagger).forEach(function(level1) {
                 //methods console.log(level2);
                 Object.keys(swagger[level1][level2]).forEach(function(level3) {
                         //get/post/delete  console.log(level3);
-                        if(swagger[level1][level2][level3]['summary'] && swagger[level1][level2][level3]['summary'].indexOf('HIDE=') == -1) 
+                        if(swagger[level1][level2][level3]['summary'] && swagger[level1][level2][level3]['summary'].indexOf('STAGE=') == -1) 
                                 {
-                                swagger[level1][level2][level3]['summary'] = 'HIDE=UNKNOWN ' + swagger[level1][level2][level3]['summary'];
+                                swagger[level1][level2][level3]['summary'] = 'STAGE=UNK' + swagger[level1][level2][level3]['summary'];
                                 }
                 });
         });
