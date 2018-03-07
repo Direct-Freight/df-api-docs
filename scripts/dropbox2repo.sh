7430255fb6c19e4e4b9930dc742c1b5f82add87a
jgabriels <jon@directfreight.com>
2018-03-07 04:52:59 -0600

    dropbox2repo

--- scripts/dropbox2repo.sh
+++ scripts/dropbox2repo.sh
@@ -3,4 +3,5 @@ cd /var/www/dfwdbi/api/df-api-docs
 if ! diff -q web_deploy/swagger-nocode.yaml /home/catalyst/Dropbox/directfreight/swagger20-with-extensions.yaml; then
         cp /home/catalyst/Dropbox/directfreight/swagger20-with-extensions.yaml restlet_studio/swagger.yaml
         npm run restlet2repo
+        git commit -m "dropbox2repo" -a
 fi
