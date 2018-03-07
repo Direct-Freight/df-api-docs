5d5a5c16fccfd59b9f3fef61a4c0423c6db9fddc
jgabriels <jon@directfreight.com>
2018-03-07 05:27:06 -0600

    dropbox2repo

--- scripts/dropbox2repo.sh
+++ scripts/dropbox2repo.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 cd /var/www/dfwdbi/api/df-api-docs
 if ! diff -q web_deploy/swagger-nocode.yaml /home/catalyst/Dropbox/directfreight/swagger20-with-extensions.yaml; then
+        cp /home/catalyst/Dropbox/directfreight/swagger20-with-extensions.yaml web_deploy/swagger-nocode.yaml
         cp /home/catalyst/Dropbox/directfreight/swagger20-with-extensions.yaml restlet_studio/swagger.yaml
         npm run restlet2repo
         git diff spec
-   if test "$1" = "PUSH"; then
         git pull --no-edit
         git commit -m "dropbox2repo" -a
         git push
-   else
-        echo "not pushing"
-   fi
 fi
