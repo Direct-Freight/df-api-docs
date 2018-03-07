cdb691cc83d67372dfbb929244c0919466eff6c5
jgabriels <jondf@directfreight.com>
2018-03-07 12:51:08 -0600

    dropbox2repo

--- scripts/dropbox2repo.sh
+++ scripts/dropbox2repo.sh
@@ -4,6 +4,8 @@ if ! diff -q web_deploy/swagger-nocode.yaml /home/catalyst/Dropbox/directfreight
         cp /home/catalyst/Dropbox/directfreight/swagger20-with-extensions.yaml web_deploy/swagger-nocode.yaml
         cp /home/catalyst/Dropbox/directfreight/swagger20-with-extensions.yaml restlet_studio/swagger.yaml
         git pull --no-edit #just in case
+        cp /home/catalyst/Dropbox/directfreight/swagger20-with-extensions.yaml web_deploy/swagger-nocode.yaml
+        cp /home/catalyst/Dropbox/directfreight/swagger20-with-extensions.yaml restlet_studio/swagger.yaml
         npm run restlet2repo
         git diff spec
         git config --global user.email "jondf@directfreight.com"
