251cd24c2d1b14f8dca32376deaf787b772acbd8
jgabriels <jon@directfreight.com>
2018-03-07 05:12:48 -0600

    added explicit push

--- scripts/dropbox2repo.sh
+++ scripts/dropbox2repo.sh
@@ -3,6 +3,10 @@ cd /var/www/dfwdbi/api/df-api-docs
 if ! diff -q web_deploy/swagger-nocode.yaml /home/catalyst/Dropbox/directfreight/swagger20-with-extensions.yaml; then
         cp /home/catalyst/Dropbox/directfreight/swagger20-with-extensions.yaml restlet_studio/swagger.yaml
         npm run restlet2repo
+   if test "$1" = "PUSH"; then
         git commit -m "dropbox2repo" -a
         git push
+   else
+        echo "not pushing"
+   fi
 fi
