2923d84a7b251524d07ff4e3e4d08eb7d1b2ed2d
jgabriels <jon@directfreight.com>
2018-03-07 04:31:14 -0600

    removed commit for debug

--- scripts/dropbox2repo.sh
+++ scripts/dropbox2repo.sh
@@ -3,5 +3,4 @@ cd /var/www/dfwdbi/api/df-api-docs
 if ! diff -q web_deploy/swagger-nocode.yaml /home/catalyst/Dropbox/directfreight/swagger20-with-extensions.yaml; then
         cp /home/catalyst/Dropbox/directfreight/swagger20-with-extensions.yaml restlet_studio/swagger.yaml
         npm run restlet2repo
-        git commit -m "auto-merge from dropbox/restlet" -a
 fi
