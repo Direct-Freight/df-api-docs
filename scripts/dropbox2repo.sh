14aea5d145997e7520bc4046e3b1f45626637b47
jgabriels <jon@directfreight.com>
2018-03-07 04:19:42 -0600

    auto-merge from dropbox/restlet

--- scripts/dropbox2repo.sh
+++ scripts/dropbox2repo.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+cd /var/www/dfwdbi/api/df-api-docs
 if ! diff -q web_deploy/swagger-nocode.yaml /home/catalyst/Dropbox/directfreight/swagger20-with-extensions.yaml; then
-        echo "different"
+        cp /home/catalyst/Dropbox/directfreight/swagger20-with-extensions.yaml restlet_studio/
+        npm run restlet2repo
+        git commit -m "auto-merge from dropbox/restlet" -a
 fi
