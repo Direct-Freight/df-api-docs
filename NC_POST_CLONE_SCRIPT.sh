1eb115132df2d73f5af802fc39b4bbf52b47a7c6
Direct-Freight <jondf@directfreight.com>
2018-03-13 12:08:55 -0500

    added script for nevercode

---
+++ NC_POST_CLONE_SCRIPT.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#Custom Build Script for Nevercode -- see https://intercom.help/nevercode/configuring-nevercode/custom-build-steps
+
+#Magic Variables to disable build for Nevercode -- see https://intercom.help/nevercode/configuring-nevercode/magic-variables
+export NC_SKIP_COCOAPODS=true  #nevercode downloading and installing Cocoapods is skipped.
+export NC_TEST_ONLY=true  #nevercode build step will be skipped.
+
+#other environment variables
+
+#Commands to do a custom build
+npm run build
+
