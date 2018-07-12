a990faf751b9ac3412c3f0f6d0388fec1c3780df
Jon Gabrielson <support@truckersearch.com>
2018-07-12 12:21:30 -0500

    dropbox2repo: support/Jon Gabrielson

--- scripts/dropbox2repo.sh
+++ scripts/dropbox2repo.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd /var/www/dfwdbi/api/df-api-docs
-export DF_APIDOCS_PATH="/home/catalyst/Dropbox/directfreight/apidocs/directfreight"
-if ! diff -q web_deploy/swagger-nocode.yaml $DF_APIDOCS_PATH/swagger20-with-extensions.yaml; then
-        cp $DF_APIDOCS_PATH/swagger20-with-extensions.yaml web_deploy/swagger-nocode.yaml
-        cp $DF_APIDOCS_PATH/swagger20-with-extensions.yaml restlet_studio/swagger.yaml
+export APIDOCS_PATH="/home/catalyst/Dropbox/directfreight/apidocs/directfreight"
+if ! diff -q web_deploy/swagger-nocode.yaml $APIDOCS_PATH/swagger20-with-extensions.yaml; then
+        cp $APIDOCS_PATH/swagger20-with-extensions.yaml web_deploy/swagger-nocode.yaml
+        cp $APIDOCS_PATH/swagger20-with-extensions.yaml restlet_studio/swagger.yaml
         git pull --no-edit #just in case
-        cp $DF_APIDOCS_PATH/swagger20-with-extensions.yaml web_deploy/swagger-nocode.yaml
-        cp $DF_APIDOCS_PATH/swagger20-with-extensions.yaml restlet_studio/swagger.yaml
+        cp $APIDOCS_PATH/swagger20-with-extensions.yaml web_deploy/swagger-nocode.yaml
+        cp $APIDOCS_PATH/swagger20-with-extensions.yaml restlet_studio/swagger.yaml
         npm run restlet2repo
         git diff spec
         export dropbox_info=`scripts/dropbox_last_modifier.pl`
