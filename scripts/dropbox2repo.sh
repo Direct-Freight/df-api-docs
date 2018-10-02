bad0a6caed18f59f87fed10bf4e6980dfdaecf04
Jon Gabrielson <support@truckersearch.com>
2018-10-02 12:15:10 -0500

    dropbox2repo: support/Jon Gabrielson

--- scripts/dropbox2repo.sh
+++ scripts/dropbox2repo.sh
@@ -2,7 +2,8 @@
 cd /var/www/dfwdbi/api/df-api-docs
 export APIDOCS_PATH="/home/catalyst/Dropbox/directfreight/apidocs/directfreight"
 if ! diff -q web_deploy/swagger-nocode.yaml $APIDOCS_PATH/swagger20-with-extensions.yaml; then
-        sed -i "s/example: [\"']\(\[.*\]\)[\"']$/example: \1/g" $APIDOCS_PATH/swagger20-with-extensions.yaml #remove extra quotes from examples
+        #sed -i "s/example: [\"']\(\[.*\]\)[\"']$/example: \1/g" $APIDOCS_PATH/swagger20-with-extensions.yaml #remove extra quotes from examples
+        perl -i ./script/clean_example.pl $APIDOCS_PATH/swagger20-with-extensions.yaml #remove extra quotes from examples
         cp $APIDOCS_PATH/swagger20-with-extensions.yaml web_deploy/swagger-nocode.yaml
         cp $APIDOCS_PATH/swagger20-with-extensions.yaml restlet_studio/swagger.yaml
         git pull --no-edit #just in case
