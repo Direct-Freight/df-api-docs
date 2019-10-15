4ae2605694bc43f0c6824f4df00fbbe48657338a
Direct-Freight <jondf@directfreight.com>
2019-10-15 14:51:00 -0500

    adding swaggerhub2repo

--- scripts/deploy-branch.js
+++ scripts/deploy-branch.js
@@ -11,6 +11,7 @@ var branch = process.env.TRAVIS_BRANCH && process.env.TRAVIS_BRANCH.toLowerCase(
 if (branch && branch !== 'gh-pages') {
   var branchPath = path.join('.tmp', 'preview', branch, '/');
   mkdir('-p', branchPath);
+  exec('npm run swaggerhub2repo');
   exec('npm run swagger bundle -- -o ' + branchPath + 'swagger.json');
   console.log("copying index.html");
   cp('web/index.html', branchPath);
