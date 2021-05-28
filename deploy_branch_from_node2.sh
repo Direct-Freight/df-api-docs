4e0bf333737c76db07fbdfefcae8caf8fb8d1364
Direct-Freight <jondf@directfreight.com>
2021-05-28 10:51:23 -0500

    try again

--- deploy_branch_from_node2.sh
+++ deploy_branch_from_node2.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 export TRAVIS_BRANCH='swaggerhub'
+git remote -v
+git remote set-url origin https://github.com/Direct-Freight/df-api-docs.git
+git remote -v
+
+../set_gh_token.df
 npm run deploy-branch
