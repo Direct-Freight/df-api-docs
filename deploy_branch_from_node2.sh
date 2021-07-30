cf1741ec255cd4bcb83e56565743c11d580a43cc
Direct-Freight <jondf@directfreight.com>
2021-07-30 09:56:39 -0500

    added branches

--- deploy_branch_from_node2.sh
+++ deploy_branch_from_node2.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-export TRAVIS_BRANCH='swaggerhub'
+if test "$1" = "swaggerhub"; then
+    export TRAVIS_BRANCH='swaggerhub'
+elif test "$1" = "master"; then
+    export TRAVIS_BRANCH='master'
+else
+    echo "invalid branch '$1'"
+    exit
+fi
+git checkout $TRAVIS_BRANCH
 git remote -v
 git remote set-url origin https://github.com/Direct-Freight/df-api-docs.git
 git remote -v
